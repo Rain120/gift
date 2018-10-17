87215f76f3a0772fe3ec824c3a5fa2b0d2dbaf4f
Rain120 <chao1202016@outlook.com>
2018-10-17 23:51:20 +0800

    planning and research the development plug-ins

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'animate.css'
+import anime from 'animejs'
 
+Vue.use(anime)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
